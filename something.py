851b8cace5bd1a51f7fbccb5e68765c203be5535
COMPANYNET\aprabhakar <kgpavinash@gmail.com>
2019-01-28 09:26:57 -0500

    first

---
+++ something.py
@@ -0,0 +1,104 @@
+import io
+import os
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+from google.cloud import storage
+# Instantiates a client
+
+# client2 = storage.Client.from_service_account_json(
+#         'service_account.json')
+#
+# client = vision.ImageAnnotatorClient()
+#
+# # The name of the image file to annotate
+# file_name = os.path.join(
+#     os.path.dirname(__file__),
+#     'risperidone-figure-03.jpg')
+#
+# # Loads the image into memory
+# with io.open(file_name, 'rb') as image_file:
+#     content = image_file.read()
+#
+# image = types.Image(content=content)
+#
+# # Performs label detection on the image file
+# response = client.label_detection(image=image)
+# labels = response.label_annotations
+#
+# print('Labels:')
+# for label in labels:
+#     print(label.description)
+
+
+# def implicit():
+#     from google.cloud import storage
+#
+#      # If you don't specify credentials when constructing the client, the
+#      # client library will look for credentials in the environment.
+#     storage_client = storage.Client.from_service_account_json(
+#         'service_account.json')
+#
+#      # Make an authenticated API request
+#     buckets = list(storage_client.list_buckets())
+#     print(buckets)
+#
+# implicit()
+#
+#
+# def explicit():
+#     from google.cloud import storage
+#
+#     # Explicitly use service account credentials by specifying the private key
+#     # file.
+#     storage_client = storage.Client.from_service_account_json(
+#         'service_account.json')
+#
+#     # Make an authenticated API request
+#     buckets = list(storage_client.list_buckets())
+#     print(buckets)
+#
+# explicit()
+
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'risperidone-figure-03.jpg')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
+
+print('Labels:')
+for label in labels:
+    print(label.description)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
