d8497f3f303459f148f9360ac17d09bf2a3912ec
COMPANYNET\aprabhakar <kgpavinash@gmail.com>
2019-01-28 15:18:12 -0500

    Process group

--- something.py
+++ something.py
@@ -1,32 +1,3 @@
-# def implicit():
-#     from google.cloud import storage
-#
-#      # If you don't specify credentials when constructing the client, the
-#      # client library will look for credentials in the environment.
-#     storage_client = storage.Client.from_service_account_json(
-#         'service_account.json')
-#
-#      # Make an authenticated API request
-#     buckets = list(storage_client.list_buckets())
-#     print(buckets)
-#
-# implicit()
-#
-#
-# def explicit():
-#     from google.cloud import storage
-#
-#     # Explicitly use service account credentials by specifying the private key
-#     # file.
-#     storage_client = storage.Client.from_service_account_json(
-#         'service_account.json')
-#
-#     # Make an authenticated API request
-#     buckets = list(storage_client.list_buckets())
-#     print(buckets)
-#
-# explicit()
-
 import io
 import os
 
@@ -38,30 +9,48 @@ from google.cloud.vision import types
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'risperidone-figure-03.jpg')
+# file_name = os.path.join(
+#     os.path.dirname(__file__),
+#     'risperidone-figure-03.jpg')
+#
+# # Loads the image into memory
+# with io.open(file_name, 'rb') as image_file:
+#     content = image_file.read()
+#
+# image = types.Image(content=content)
+#
+# # Performs label detection on the image file
+# response = client.label_detection(image=image)
+# labels = response.label_annotations
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
 
-image = types.Image(content=content)
+# Assuming there are not subdirectories, only image files
+files = []
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+for (dirpath, dirnames, filenames) in os.walk(r'C:\Users\aprabhakar\Desktop\snakes\testDAT\images'):
+    for f in filenames:
+        files.append(os.path.join(dirpath, f))
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+for file in files:
+    with io.open(file, 'rb') as image_file:
+        content = image_file.read()
 
-print()
+    print("PRINT STATEMENT: NEW IMAGE BEING PROCESSED")
 
-resp = client.document_text_detection(image=image)
-# print('\n'.join([d.description for d in resp.text_annotations]))
-for d in resp.text_annotations:
-    print(d.description)
+    image = types.Image(content=content)
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    # print('Labels:')
+    # for label in labels:
+    #     print(label.description)
+
+    resp = client.document_text_detection(image=image)
+# # print('\n'.join([d.description for d in resp.text_annotations]))
+    for d in resp.text_annotations:
+        print(d.description)
+        break
+
+
 
 
 
