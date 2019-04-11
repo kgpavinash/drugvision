e662adf3486a1ac1c92df7b89969cebf554f6e4b
COMPANYNET\aprabhakar <kgpavinash@gmail.com>
2019-04-11 13:54:37 -0400

    minor change

--- ExtractFromText.py
+++ ExtractFromText.py
@@ -4,19 +4,30 @@ import re
 directory = r"C:\Users\aprabhakar\Desktop\snakes\sorter"
 os.chdir(directory)
 
-f = open("predictions2.jsonl")
-for line in f:
-    print(line)
-    getOther = re.search("STORAGE(.[^}]*)", line)
-    break
-
-print(getOther[0])
-getOtherScore = re.search("\d+\.\d+", getOther[0])
-print(getOtherScore[0])
-# ans = re.findall("\d+\.\d+", getOther[0])
-# print(ans[0])
+f = open("FinalText.txt", encoding="utf8")
+f2 = open("Excellines.txt", "w+", encoding="utf8")
+f3 = open("lastlines.txt", encoding="utf8")
+for line, line2 in zip(f,f3):
+    getLine = re.search("Line(.[^,]*)", line)
+    getLineNumber = re.search("\d+", getLine[0])
+    getOther = re.search("OTHER(.[^}]*)", line)
+    getStorage = re.search("STORAGE(.[^}]*)", line)
+    getOtherScore = re.search("\d+\.\d+", getOther[0])
+    getStorageScore = re.search("\d+\.\d+", getStorage[0])
+    # getRealData = line.split(",")[-1]
+    # excelLine = getLineNumber[0] + '\t' + getOtherScore[0] + '\t' + getStorageScore[0] + '\t' + getRealData + '\t\n'
+    # print(excelLine)
+    # print(getRealData)
+    excelLine = getLineNumber[0] + '\t' + getOtherScore[0] + '\t' + getStorageScore[0] + '\t' + line2
+    f2.write(excelLine)
+    # f2.write(getLineNumber[0])
+    # f2.write('\t')
+    # f2.write(getOtherScore[0])
+    # f2.write('\t')
+    # f2.write(getStorageScore[0])
 
 
 f.close()
+f2.close()
 
 # OTHER(.[^}]*)
